0a5cf528f7d08add874f40839001b29b4afb60d1
kimonuduo <lyc0128@gmail.com>
2016-12-26 10:38:53 +0800

    fix notification

--- client/app/activity_detail/ad_participates_item.js
+++ client/app/activity_detail/ad_participates_item.js
@@ -3,6 +3,8 @@ import {hashHistory} from 'react-router';
 import {addFriend} from '../server';
 import {hideElement} from '../util';
 import {socket,getToken} from '../credentials';
+ var debug = require('react-debug');
+
 
 export default class Ad_participates_item extends React.Component{
   constructor(props){
@@ -15,7 +17,7 @@ export default class Ad_participates_item extends React.Component{
 
   handleRedirect(e){
     e.preventDefault();
-    hashHistory.push("profile/"+this.state._id);
+    hashHistory.push("profile/"+this.props.data._id);
   }
 
   checkFriendsOfUser(){
@@ -23,7 +25,7 @@ export default class Ad_participates_item extends React.Component{
       return false;
     }
     return this.props.friends.filter((friend)=>{
-      if(friend._id===this.state.data._id)
+      if(friend._id===this.props.data._id)
         return true;
       else return false;
     }).length>0;
@@ -31,16 +33,19 @@ export default class Ad_participates_item extends React.Component{
 
   handleAddFriend(e){
     e.preventDefault();
+      debug("----gg---------------------------------------this.props.currUser")
     addFriend(this.props.currUser,this.props.data._id,(success)=>{
       if(success){
-        this.setState({
-          success:true
-        });
-
+        debug("-------------------------------------------this.props.currUser")
+        debug(this.props.currUser)
         socket.emit('notification',{
           authorization:getToken(),
+
           sender: this.props.currUser,
-          target: this.state.data._id
+          target: this.props.data._id
+        });
+        this.setState({
+          success:true
         });
       }
     });
@@ -52,15 +57,15 @@ export default class Ad_participates_item extends React.Component{
         <div className={"alert alert-success "+hideElement(!this.state.success)} role="alert">Request sent!</div>
           <div className="media-left">
             <a onClick={(e)=>this.handleRedirect(e)} data-dismiss="modal" aria-label="Close">
-              <img className="media-object" src={this.state.data.avatar} height="55px" alt="..."/>
+              <img className="media-object" src={this.props.data.avatar} height="55px" alt="..."/>
             </a>
           </div>
           <div className="media-body media-top">
-            {this.state.data.fullname}<br/>
-            {this.state.data.ps}
+            {this.props.data.fullname}<br/>
+            {this.props.data.description}
         </div>
         <div className="media-body media-right" style={{textAlign:"right"}} >
-          <a href="#" onClick={(e)=>this.handleAddFriend(e)}><i className={"fa fa-user-plus pull-right "+hideElement(this.checkFriendsOfUser()||this.state.data._id===this.props.currUser)} style={{'paddingRight':'20px'}} aria-hidden="true"></i></a>
+          <a href="#" onClick={(e)=>this.handleAddFriend(e)}><i className={"fa fa-user-plus pull-right "+hideElement(this.checkFriendsOfUser()||this.props.data._id===this.props.currUser)} style={{'paddingRight':'20px'}} aria-hidden="true"></i></a>
           <i className={"fa fa-check pull-right "+hideElement(!this.checkFriendsOfUser())} style={{color:'green','paddingRight':'20px',textAlign:"right"}} aria-hidden="true"></i>
         </div>
       </li>
