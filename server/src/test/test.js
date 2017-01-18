7a7168757a0f436043f774a9d19f98fb89ef2cf0
shengbolou <loushengbo@hotmail.com>
2017-01-17 19:56:29 -0500

    create test file

---
+++ server/src/test/test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var describe = require('mocha').describe;
+var it = require('mocha').it;
+var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    assert.equal(0, 0);
+  });
+});
