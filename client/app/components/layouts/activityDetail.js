2f5c10922b75d2cf5d80a83c034ced2548aca8e2
Shengbo Lou <loushengbo@hotmail.com>
2018-09-01 13:08:31 -0400

    test git

--- client/app/components/layouts/activityDetail.js
+++ client/app/components/layouts/activityDetail.js
@@ -5,7 +5,7 @@ import {ActivityDetailBody} from '../containers';
 export default class ActivityDetail extends React.Component{
 
     constructor(props){
-        super(props);
+        super(props); 
     }
 
     render(){
